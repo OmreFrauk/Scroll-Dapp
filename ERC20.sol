0989f046223ef830954c0866b255912befb540c9
OmreFrauk <117479359+OmreFrauk@users.noreply.github.com>
2024-06-28 12:27:42 +0300

    first commit

---
+++ ERC20.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract TokenSale is ERC20 {
+    constructor(
+        uint256 _initialSuppply,
+        string memory _tokenName,
+        string memory _tokenSymbol
+    ) ERC20(_tokenName, _tokenSymbol) {
+        _mint(msg.sender, _initialSuppply);
+    }
+}
